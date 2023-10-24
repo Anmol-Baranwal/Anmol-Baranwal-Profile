a04a7c85da80923c1344af99999c771c36eb1ea0
Anmol Baranwal <anmolbaranwal119@gmail.com>
2023-10-24 19:01:19 +0530

    chore: separate profile content for other options of nav

--- lib/src/screens/profile_screen.dart
+++ lib/src/screens/profile_screen.dart
@@ -39,67 +39,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 width: double.infinity, // Takes up the whole width
               ),
             ),
-            Transform.translate(
-              offset: Offset(0, -75), // this is as per the banner
-              child: ProfilePictureWidget(),
-            ),
-            Container(
-              margin:
-                  EdgeInsets.only(top: 0), // Adjust the top margin as needed
-              child: Text(
-                'Anmol Baranwal',
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center, // Center-align the text
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 2, bottom: 0),
-              child: Text(
-                'Haryana, India',
-                style: TextStyle(
-                  fontSize: 14,
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            ColumnWithDivider(
-              items: [
-                ColumnWithDividerItem(text: '20+', subtext: 'Projects'),
-                ColumnWithDividerItem(
-                    text: '150+', subtext: 'Followers (GitHub)'),
-                ColumnWithDividerItem(text: '40k+', subtext: 'Blog Views'),
-              ],
-            ),
-            SizedBox(height: 20),
-            Container(
-              margin: EdgeInsets.only(
-                  top: 20, bottom: 15), // Adjust the margin as needed
-              child: Text(
-                'Crafting products with Open Source at heart',
-                style: TextStyle(
-                  fontSize: 14,
-                ),
-              ),
-            ),
-            // here we need to add
-            SectionSeparator(),
-            InfoRow(
-              heading1: 'Email',
-              subtext1: 'anmolbaranwal119@gmail.com',
-              heading2: 'Born',
-              subtext2: '9th November 2001',
-              heading3: 'Phone',
-              subtext3: '+91 8931073435',
-            ),
-            AboutMeContainer(
-              title: 'About Me',
-              content:
-                  'Loving open source, I\'m a passionate full-stack developer in the top 8% on GitHub, actively contributing to communities and maintaining @LinksHub. With a rank of 11 in @GSSoC\'23, I also served as a core team member and mentor. My technical writing has garnered over 40k+ views, and I recently completed Buildspace N&W S4, building designlyfe.tech.',
-            ),
-            SocialMediaRow(),
             _tabs[_currentTab],
           ],
         ),
@@ -137,8 +76,69 @@ class _ProfileScreenState extends State<ProfileScreen> {
 class ProfileContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Content'),
+    return Column(
+      children: [
+        // Banner Image
+        Transform.translate(
+          offset: Offset(0, -75), // this is as per the banner
+          child: ProfilePictureWidget(),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 0), // Adjust the top margin as needed
+          child: Text(
+            'Anmol Baranwal',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center, // Center-align the text
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 2, bottom: 0),
+          child: Text(
+            'Haryana, India',
+            style: TextStyle(
+              fontSize: 14,
+            ),
+          ),
+        ),
+        SizedBox(height: 20),
+        ColumnWithDivider(
+          items: [
+            ColumnWithDividerItem(text: '20+', subtext: 'Projects'),
+            ColumnWithDividerItem(text: '150+', subtext: 'Followers (GitHub)'),
+            ColumnWithDividerItem(text: '40k+', subtext: 'Blog Views'),
+          ],
+        ),
+        SizedBox(height: 20),
+        Container(
+          margin: EdgeInsets.only(
+              top: 20, bottom: 15), // Adjust the margin as needed
+          child: Text(
+            'Crafting products with Open Source at heart',
+            style: TextStyle(
+              fontSize: 14,
+            ),
+          ),
+        ),
+        // here we need to add
+        SectionSeparator(),
+        InfoRow(
+          heading1: 'Email',
+          subtext1: 'anmolbaranwal119@gmail.com',
+          heading2: 'Born',
+          subtext2: '9th November 2001',
+          heading3: 'Phone',
+          subtext3: '+91 8931073435',
+        ),
+        AboutMeContainer(
+          title: 'About Me',
+          content:
+              'Loving open source, I\'m a passionate full-stack developer in the top 8% on GitHub, actively contributing to communities and maintaining @LinksHub. With a rank of 11 in @GSSoC\'23, I also served as a core team member and mentor. My technical writing has garnered over 40k+ views, and I recently completed Buildspace N&W S4, building designlyfe.tech.',
+        ),
+        SocialMediaRow(),
+      ],
     );
   }
 }
